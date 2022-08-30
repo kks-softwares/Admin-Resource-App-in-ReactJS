ae49fcdd181494e222cc117303b54476313410c7
Rahul Rajput <76735555+RahulRaj352001@users.noreply.github.com>
2022-08-31 01:13:08 +0530

    job detail page

--- src/components/DashBoardSidebar/UserFullDetail/JobDetail/Jobdetail.jsx
+++ src/components/DashBoardSidebar/UserFullDetail/JobDetail/Jobdetail.jsx
@@ -1,7 +1,154 @@
-import React from 'react'
-import './jobdetail.css'
+import React, { useState } from "react";
+import "./jobdetail.css";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import CloseIcon from "@mui/icons-material/Close";
+import { useNavigate } from "react-router";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 export default function Jobdetail() {
+  const [down1, setDown1] = useState(false);
+  const [down2, setDown2] = useState(false);
+  const [down3, setDown3] = useState(false);
+  const [down4, setDown4] = useState(false);
+  const [longofproposallist, setLongofproposallist] = useState(["1","2"])
   return (
-    <div>Jobdetail</div>
-  )
+    <div>
+      <div
+        style={{
+          width: "100%",
+          background: "white",
+          padding: "2vw 1vw",
+          margin: "1vw 0vw",
+        }}
+      >
+        {" "}
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div className="tagblue">Mobile Application</div>
+            <div style={{ marginLeft: "1vw", fontSize: "0.9vw" }}>
+              Mobile Application
+            </div>
+          </div>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                margin: "0",
+                height: "2.4vw",
+                background: "#E4E4E4",
+                fontSize: "400",
+              }}
+              className="digitalwallate"
+            >
+              <span
+                style={{
+                  padding: "0.6vw 0.5vw",
+                  fontSize: "400",
+                  background: "none",
+                }}
+              >
+                Chat to Client
+              </span>
+            </div>
+          </div>
+        </div>{" "}
+        <div
+          style={{
+            fontWeight: "600",
+            fontSize: "1.3vw",
+            display: "flex",
+            flexWrap: "wrap",
+            marginTop: "1vw",
+            marginLeft: "1vw",
+            color: "#064C87",
+          }}
+        >
+          Senior Product Designer (#34793)
+        </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <span>
+            <LocationOnOutlinedIcon
+              style={{
+                fontSize: "1.5vw",
+                fontWeight: "400",
+                margin: "0.5vw 1vw",
+              }}
+            />
+          </span>
+          <span style={{ fontSize: "1.1vw", fontWeight: "500" }}>
+            {"Remote Kanpur"}
+          </span>
+        </div>
+        <div
+          style={{
+            width: "100%",
+            margin: "0.8vw 1vw",
+            fontSize: "0.85vw",
+            marginBottom: "0.0vw",
+            marginRight: "2vw",
+          }}
+          className="dashboardtitilemainparabid"
+        >
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum. the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum.
+        </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <span style={{ fontSize: "1.1vw", fontWeight: "500", margin: "1vw" }}>
+            {"Skills"}
+          </span>
+        </div>
+        <div
+          style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}
+        >
+          <div className="skillmap">Usability Testing</div>
+          <div className="skillmap">Prototyping</div>
+          <div className="skillmap">Prototyping</div>
+          <div className="skillmap">User interface</div>
+          <div className="skillmap">Prototyping</div>
+        </div>
+        <div style={{height:down1? `${(longofproposallist?.length*4)+6}vw`:""}} className="boxofextension">
+          <div className="flexofboxextentionnav">
+            <div>List of Proposal</div>
+            <div
+              onClick={() => {
+                setDown1(!down1);
+              }}
+            >
+              {!down1 ? (
+                <KeyboardArrowDownIcon
+                  style={{ fontSize: "2vw", margin: "1vw",cursor:"pointer" }}
+                />
+              ) : (
+                <KeyboardArrowUpIcon
+                  style={{ fontSize: "2vw", margin: "1vw",cursor:"pointer" }}
+                />
+              )}
+            </div>
+          </div>
+            <div hidden={!down1} >
+              {longofproposallist?.map((data)=>{
+                  return <div  className="listofproposalname" >data</div>
+              })}
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 }
